9f1a9cdadded04c85cfe3e0019cbfa72f142ba83
chnaaam <a71335976@gmail.com>
2022-02-17 17:51:10 +0900

    add: deepspeed library

--- datagene/tasks/semantic_role_labeling.py
+++ datagene/tasks/semantic_role_labeling.py
@@ -62,13 +62,20 @@ class SRL(TaskBase):
                     crf_masks=(token_tensor != self.token_pad_id)
                 )
                 
-                loss.backward()
-                self.optimizer.step()
+                if not self.use_deepspeed_lib:
+                    loss.backward()
+                    self.optimizer.step()
+                
+                else:
+                    self.model.backward(loss)
+                    self.model.step()                
+                
                 train_losses.append(loss.item())
                 
                 avg_train_loss = sum(train_losses) / len(train_losses)
             
-            self.scheduler.step()
+            if not self.use_deepspeed_lib:
+                self.scheduler.step()
             
             avg_valid_loss, avg_valid_f1_score = self.valid()
             
