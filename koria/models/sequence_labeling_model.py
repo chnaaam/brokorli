51321d7ec90c21b4a892852bfe02c4041cff3aa5
chnaaam <a71335976@gmail.com>
2022-03-10 23:56:48 +0900

    fix sequence labeling model

--- koria/models/sequence_labeling_model.py
+++ koria/models/sequence_labeling_model.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 from transformers import AutoConfig, AutoModelForTokenClassification
 
 class SequenceLabelingModel(nn.Module):
-    def __init__(self, model_name, parameters):
+    def __init__(self, model_name, num_labels, vocab_size=None):
         super().__init__()
         
         self.model_name = model_name
         
-        config = AutoConfig.from_pretrained(model_name)
-        config.num_labels = parameters["label_size"]
+        config = AutoConfig.from_pretrained(model_name, num_labels=num_labels)
         
         self.model = AutoModelForTokenClassification.from_config(config)
-        self.model.resize_token_embeddings(parameters["vocab_size"])
         
-    def forward(self, X, **parameters):
+        if vocab_size:
+            self.model.resize_token_embeddings(vocab_size)
+        
+    def forward(self, **parameters):
+        input_ids = parameters["input_ids"]
         token_type_ids = parameters["token_type_ids"]
         attention_mask = parameters["attention_mask"]
         labels = parameters["labels"]
@@ -22,14 +24,14 @@ class SequenceLabelingModel(nn.Module):
         # TODO: Roberta does not need token_type_ids
         if "roberta" not in self.model_name:
             outputs = self.model(
-                X,
+                input_ids,
                 token_type_ids=token_type_ids,
                 attention_mask=attention_mask,
                 labels=labels,
             )
         else:
             outputs = self.model(
-                X,
+                input_ids,
                 attention_mask=attention_mask,
                 labels=labels,
             )
